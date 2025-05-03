579fc7a7f049918a0e2060ca7b28d242593568ff
Javier Ochoa <118292286+Javierochoa159@users.noreply.github.com>
2025-05-02 22:59:52 -0300

    remove trash

--- app/Views/SuccessLogin.php
+++
@@ -1,9 +0,0 @@
-<html><head><title>My Form</title></head><body>
-<h3>Envío de Formulario Exitoso!</h3>
-<?php
-echo "Correo: ".$email."<br>";
-echo "Password: ".$password."<br>";
-?>
-<p>
-<?php echo anchor('form', 'Intentar nuevamente!'); ?>
-</p> </body> </html>
